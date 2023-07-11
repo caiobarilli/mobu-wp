39882a5ea4b6b25bf01fc377fb680956f0bdc8e3
Caio Barilli <caiobarilli@gmail.com>
2023-07-11 01:21:32 -0300

    [frontend] about full desktop & mobile

--- cms_mobu/wp-content/themes/mobu_theme/navbar.php
+++ cms_mobu/wp-content/themes/mobu_theme/navbar.php
@@ -31,6 +31,23 @@
 
             ?>
 
+            <?php if (wp_is_mobile()) : ?>
+                <button type="button" data-bs-toggle="collapse" data-bs-target="#menu-mobile" aria-expanded="true" aria-controls="menu-mobile">
+                    <svg viewBox="0 0 20 20" height="48" width="48" focusable="false" role="img" fill="#ffffff" xmlns="http://www.w3.org/2000/svg">
+                        <title>Menu icon</title>
+                        <path d="M16.4 9H3.6c-.552 0-.6.447-.6 1 0 .553.048 1 .6 1h12.8c.552 0 .6-.447.6-1 0-.553-.048-1-.6-1zm0 4H3.6c-.552 0-.6.447-.6 1 0 .553.048 1 .6 1h12.8c.552 0 .6-.447.6-1 0-.553-.048-1-.6-1zM3.6 7h12.8c.552 0 .6-.447.6-1 0-.553-.048-1-.6-1H3.6c-.552 0-.6.447-.6 1 0 .553.048 1 .6 1z"></path>
+                    </svg>
+                </button>
+            <?php endif; ?>
         </div>
     </div>
+
+    <?php if (wp_is_mobile()) : ?>
+        <div id="menu-mobile" class="accordion-collapse collapse" aria-labelledby="menu-mobile">
+            <div class="wrap-content">
+                <?php main_nav(); ?>
+                <?php social_nav(); ?>
+            </div>
+        </div>
+    <?php endif; ?>
 </nav>
