f5ae1de5cea9da320595e6ff804e343f2b91e49c
Caio Barilli <caiobarilli@gmail.com>
2023-08-03 13:57:33 -0300

    [fix] ajusta erro no script

--- cms_mobu/wp-content/themes/mobu_theme/assets/scripts/home.js
+++ cms_mobu/wp-content/themes/mobu_theme/assets/scripts/home.js
@@ -135,236 +135,236 @@ $.when($.ready).then(function () {
       });
     });
   }
-});
 
-/**
- * Video (play/pause actions)
- */
-if ($(".wrap-video-cta").length) {
-  const videoElement = document.querySelector(".video-cta");
+  /**
+   * Video (play/pause actions)
+   */
+  if ($(".wrap-video-cta").length) {
+    const videoElement = document.querySelector(".video-cta");
 
-  videoElement.addEventListener("click", () => {
-    if (videoElement.paused) {
-      videoElement.play();
-    } else {
-      videoElement.pause();
-    }
-  });
-}
-
-/**
- * Testimonials slider
- */
-if ($(".testimonials-slider").length) {
-  const swiperTestimonials = new Swiper(".testimonials-slider", {
-    modules: [Navigation, Pagination],
-
-    pagination: {
-      el: ".swiper-pagination-testimonials",
-      clickable: true,
-    },
-  });
-}
-
-/**
- * Team content
- */
-if ($(".team").length) {
-  const wrapDevs = document.querySelectorAll(".wrap-dev");
-
-  wrapDevs.forEach((wrapDev) => {
-    const frontTeam = wrapDev.querySelector(".front-team"),
-      backTeam = wrapDev.querySelector(".back-team"),
-      icon = wrapDev.querySelector(".ico-team");
-
-    wrapDev.addEventListener("mouseover", () => {
-      icon.classList.add("hide-anim");
-      frontTeam.classList.add("hide-anim");
-      backTeam.classList.add("show-anim");
+    videoElement.addEventListener("click", () => {
+      if (videoElement.paused) {
+        videoElement.play();
+      } else {
+        videoElement.pause();
+      }
     });
+  }
 
-    wrapDev.addEventListener("mouseout", () => {
-      icon.classList.remove("hide-anim");
-      icon.classList.add("show-anim");
+  /**
+   * Testimonials slider
+   */
+  if ($(".testimonials-slider").length) {
+    const swiperTestimonials = new Swiper(".testimonials-slider", {
+      modules: [Navigation, Pagination],
 
-      backTeam.classList.remove("show-anim");
-
-      frontTeam.classList.remove("hide-anim");
-      frontTeam.classList.add("show-anim");
+      pagination: {
+        el: ".swiper-pagination-testimonials",
+        clickable: true,
+      },
     });
-  });
-}
+  }
 
-/**
- * News
- */
-if ($(".news").length) {
-  const singleNews = document.querySelectorAll(".single-news"),
-    singlePostNews = document.querySelector(".single-post-news"),
-    backdrop = document.querySelector(".backdrop-post-news"),
-    singlePost = document.querySelector(".wrap-single-post");
+  /**
+   * Team content
+   */
+  if ($(".team").length) {
+    const wrapDevs = document.querySelectorAll(".wrap-dev");
 
-  backdrop.addEventListener("click", () => {
-    singlePostNews.style.display = "none";
-  });
+    wrapDevs.forEach((wrapDev) => {
+      const frontTeam = wrapDev.querySelector(".front-team"),
+        backTeam = wrapDev.querySelector(".back-team"),
+        icon = wrapDev.querySelector(".ico-team");
 
-  singleNews.forEach((single) => {
-    const content = single.querySelector(".news-content"),
-      postId = single.querySelector('input[name="post_id"]').value;
+      wrapDev.addEventListener("mouseover", () => {
+        icon.classList.add("hide-anim");
+        frontTeam.classList.add("hide-anim");
+        backTeam.classList.add("show-anim");
+      });
 
-    content.addEventListener("click", () => {
-      $.ajax({
-        url: wp.ajax_url,
-        type: "POST",
-        data: {
-          action: "show_post",
-          postId: postId,
-        },
-        success: function (response) {
-          if (response) {
-            singlePost.innerHTML = response;
-            setTimeout(() => {
-              $.smoothScroll({
-                scrollTarget: "#news",
-                speed: 600,
-              });
-              singlePostNews.style.display = "block";
-            }, 400);
-          }
-        },
+      wrapDev.addEventListener("mouseout", () => {
+        icon.classList.remove("hide-anim");
+        icon.classList.add("show-anim");
+
+        backTeam.classList.remove("show-anim");
+
+        frontTeam.classList.remove("hide-anim");
+        frontTeam.classList.add("show-anim");
       });
     });
-  });
-}
+  }
 
-/**
- * Single post News
- */
-if ($(".wrap-single-post").length) {
-  const wrapSinglePost = document.querySelector(".wrap-single-post");
+  /**
+   * News
+   */
+  if ($(".news").length) {
+    const singleNews = document.querySelectorAll(".single-news"),
+      singlePostNews = document.querySelector(".single-post-news"),
+      backdrop = document.querySelector(".backdrop-post-news"),
+      singlePost = document.querySelector(".wrap-single-post");
 
-  const observer = new MutationObserver(function (mutations) {
-    mutations.forEach(function (mutation) {
-      // Verificar se a div "wrap-single-post" foi modificada
-      if (mutation.target === wrapSinglePost) {
-        const singlePostNews = document.querySelector(".single-post-news"),
-          singlePost = document.querySelector(".wrap-single-post");
-
-        const prevNews = document.querySelector(".previous-post"),
-          nextNews = document.querySelector(".next-post");
-
-        const prevId = document.querySelector(
-          '.previous-post > form > input[name="post_id"]'
-        ).value;
-
-        const nextId = document.querySelector(
-          '.next-post > form > input[name="post_id"]'
-        ).value;
-
-        const close = document.querySelectorAll(".close-post");
-
-        // Close
-        close.forEach((x) => {
-          x.addEventListener("click", () => {
-            singlePostNews.style.display = "none";
-          });
-        });
-
-        // Prev
-        prevNews.addEventListener("click", () => {
-          setTimeout(() => {
-            singlePostNews.style.display = "none";
-          }, 200);
-
-          if (prevId) {
-            $.ajax({
-              url: wp.ajax_url,
-              type: "POST",
-              data: {
-                action: "show_post",
-                postId: prevId,
-              },
-              success: function (response) {
-                if (response) {
-                  singlePost.innerHTML = response;
-                  setTimeout(() => {
-                    $.smoothScroll({
-                      scrollTarget: "#news",
-                      speed: 600,
-                    });
-                    singlePostNews.style.display = "block";
-                  }, 400);
-                }
-              },
-            });
-          }
-        });
-
-        // Next
-        nextNews.addEventListener("click", () => {
-          if (nextId) {
-            $.ajax({
-              url: wp.ajax_url,
-              type: "POST",
-              data: {
-                action: "show_post",
-                postId: nextId,
-              },
-              success: function (response) {
-                if (response) {
-                  singlePostNews.style.display = "block";
-                  setTimeout(() => {
-                    $.smoothScroll({
-                      scrollTarget: "#news",
-                      speed: 600,
-                    });
-                    singlePost.innerHTML = response;
-                  }, 400);
-                }
-              },
-            });
-          }
-        });
-      }
+    backdrop.addEventListener("click", () => {
+      singlePostNews.style.display = "none";
     });
-  });
 
-  const options = {
-    childList: true,
-    subtree: false,
-  };
+    singleNews.forEach((single) => {
+      const content = single.querySelector(".news-content"),
+        postId = single.querySelector('input[name="post_id"]').value;
 
-  observer.observe(wrapSinglePost, options);
-}
+      content.addEventListener("click", () => {
+        $.ajax({
+          url: wp.ajax_url,
+          type: "POST",
+          data: {
+            action: "show_post",
+            postId: postId,
+          },
+          success: function (response) {
+            if (response) {
+              singlePost.innerHTML = response;
+              setTimeout(() => {
+                $.smoothScroll({
+                  scrollTarget: "#news",
+                  speed: 600,
+                });
+                singlePostNews.style.display = "block";
+              }, 400);
+            }
+          },
+        });
+      });
+    });
+  }
 
-/**
- * Share post News
- */
-if ($("#share-post").length) {
-  const postId = document.querySelector(
-      '#share-post input[name="post_id"]'
-    ).value,
-    singlePostNews = document.querySelector(".single-post-news"),
-    singlePost = document.querySelector(".wrap-single-post");
+  /**
+   * Single post News
+   */
+  if ($(".wrap-single-post").length) {
+    const wrapSinglePost = document.querySelector(".wrap-single-post");
 
-  $.ajax({
-    url: wp.ajax_url,
-    type: "POST",
-    data: {
-      action: "show_post",
-      postId: postId,
-    },
-    success: function (response) {
-      if (response) {
-        singlePost.innerHTML = response;
-        setTimeout(() => {
-          $.smoothScroll({
-            scrollTarget: "#news",
-            speed: 600,
+    const observer = new MutationObserver(function (mutations) {
+      mutations.forEach(function (mutation) {
+        // Verificar se a div "wrap-single-post" foi modificada
+        if (mutation.target === wrapSinglePost) {
+          const singlePostNews = document.querySelector(".single-post-news"),
+            singlePost = document.querySelector(".wrap-single-post");
+
+          const prevNews = document.querySelector(".previous-post"),
+            nextNews = document.querySelector(".next-post");
+
+          const prevId = document.querySelector(
+            '.previous-post > form > input[name="post_id"]'
+          ).value;
+
+          const nextId = document.querySelector(
+            '.next-post > form > input[name="post_id"]'
+          ).value;
+
+          const close = document.querySelectorAll(".close-post");
+
+          // Close
+          close.forEach((x) => {
+            x.addEventListener("click", () => {
+              singlePostNews.style.display = "none";
+            });
           });
-          singlePostNews.style.display = "block";
-        }, 400);
-      }
-    },
-  });
-}
+
+          // Prev
+          prevNews.addEventListener("click", () => {
+            setTimeout(() => {
+              singlePostNews.style.display = "none";
+            }, 200);
+
+            if (prevId) {
+              $.ajax({
+                url: wp.ajax_url,
+                type: "POST",
+                data: {
+                  action: "show_post",
+                  postId: prevId,
+                },
+                success: function (response) {
+                  if (response) {
+                    singlePost.innerHTML = response;
+                    setTimeout(() => {
+                      $.smoothScroll({
+                        scrollTarget: "#news",
+                        speed: 600,
+                      });
+                      singlePostNews.style.display = "block";
+                    }, 400);
+                  }
+                },
+              });
+            }
+          });
+
+          // Next
+          nextNews.addEventListener("click", () => {
+            if (nextId) {
+              $.ajax({
+                url: wp.ajax_url,
+                type: "POST",
+                data: {
+                  action: "show_post",
+                  postId: nextId,
+                },
+                success: function (response) {
+                  if (response) {
+                    singlePostNews.style.display = "block";
+                    setTimeout(() => {
+                      $.smoothScroll({
+                        scrollTarget: "#news",
+                        speed: 600,
+                      });
+                      singlePost.innerHTML = response;
+                    }, 400);
+                  }
+                },
+              });
+            }
+          });
+        }
+      });
+    });
+
+    const options = {
+      childList: true,
+      subtree: false,
+    };
+
+    observer.observe(wrapSinglePost, options);
+  }
+
+  /**
+   * Share post News
+   */
+  if ($("#share-post").length) {
+    const postId = document.querySelector(
+        '#share-post input[name="post_id"]'
+      ).value,
+      singlePostNews = document.querySelector(".single-post-news"),
+      singlePost = document.querySelector(".wrap-single-post");
+
+    $.ajax({
+      url: wp.ajax_url,
+      type: "POST",
+      data: {
+        action: "show_post",
+        postId: postId,
+      },
+      success: function (response) {
+        if (response) {
+          singlePost.innerHTML = response;
+          setTimeout(() => {
+            $.smoothScroll({
+              scrollTarget: "#news",
+              speed: 600,
+            });
+            singlePostNews.style.display = "block";
+          }, 400);
+        }
+      },
+    });
+  }
+});
